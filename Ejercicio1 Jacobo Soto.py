a8ab48efec67a67eba747811a6e7bbd321b1b7a8
jacobosoto777 <105400486+jacobosoto777@users.noreply.github.com>
2022-05-11 20:09:25 -0500

    Add files via upload

---
+++ Ejercicio1 Jacobo Soto.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print ("Realizado por: Jacobo Soto ")
+print()
+##Consulta de valores
+print ("Consutando tipo de valores:")
+print()
+##Consulta de tipo de valor
+print("El tipo de dato de 875 es:")
+print(type(820))
+print()
+##Consulta de tipo de valor
+print("El tipo de dato de 4.89 es:")
+print(type(4.89))
+print()
+##Consulta de tipo de valor
+print("EEl tipo de dato del texto: Now is better than never. es:")
+print(type("Now is better than never."))
+print()
+##Consulta de tipo de valor
+print("El tipo de dato de 1.32 es:")
+print(type(1.32))
+print()
+##Consulta de tipo de valor de una operacion
+print("¿El valor 5 + 8 corresponde a un valor entero?")
+suma=8+5
+print(suma)
+print(type(suma))
+print()
+##Consulta de tipo de valor para determinar si es String
+print("¿El texto: Readability counts. corresponde a un string?")
+print(isinstance("Readability counts. corresponde a un string",str))
+##Fin
+
+
+
+
+
+
+
+
+
+
+
---
+++ Ejercicio2 Jacobo Soto.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 11 18:16:44 2022
+
+@author: jacob
+"""
+
+##Programa que identifica el tipo de dato de un valor ingresado por el usuario, se realizarán cinco interacciones:
+print("Programa que identifica el tipo de dato de un valor ingresado por el usuario, se realizarán cinco interacciones:")    
+print()
+print()
+##Primera Interacción, ingrese un valor cualquiera: (El usuario debe ingresar un valor)
+dato1=input("Primera Interacción, ingrese un valor cualquiera:")
+print(type(dato1))
+print()
+##Segunda Interacción, ingrese un valor cualquiera: (El usuario debe ingresar un valor)
+dato2=input("Segunda Interacción, ingrese un valor cualquiera:")
+print(type(dato2))
+print()
+##Tercera Interacción, ingrese un valor cualquiera: (El usuario debe ingresar un valor)
+dato3=input("Tercera Interacción, ingrese un valor cualquiera:")
+print(type(dato3))
+print()
+##Cuarta Interacción, ingrese un valor cualquiera: (El usuario debe ingresar un valor)
+dato4=input("Cuarta Interacción, ingrese un valor cualquiera:")
+print(type(dato4))
+print()
+##Quinta Interacción, ingrese un valor cualquiera: (El usuario debe ingresar un valor)
+dato5=input("Quinta Interacción, ingrese un valor cualquiera:")
+print(type(dato5))
+print()
+print("¡YA NO SE HARÁN MÁS INTERACCIONES!") 
+##FIN
+
+
+      
